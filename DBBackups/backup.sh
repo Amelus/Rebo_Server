51ddd2e8e24276bac60a3505eaace6d1c0dfd53a
Amel Hamidovic <hamidovic@student.tugraz.at>
2021-03-06 23:01:08 +0100

    Added conversation backup automatisation scripts and similar

--- DBBackups/backup.sh
+++ DBBackups/backup.sh
@@ -1,1 +1,1 @@
- docker exec nodechat /usr/bin/mysqldump -u root --password=smoot nodechat > /home/ahamidovic/Rebo_Server/DBBackups/$(date +%Y-%m-%d_%H:%M).sql 
+ docker exec nodechat /usr/bin/mysqldump -u root --password=smoot nodechat > $HOME/Rebo_Server/DBBackups/$(date +%Y-%m-%d_%H:%M).sql
---
+++ DBBackups/save_conversations.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+backupDir="$HOME/Rebo_Server/DBBackups/backup-$(date +%Y-%m-%d)"
+uploadDir="/field_study_3/Conversations_2021/$(date +%Y-%m-%d)"
+uploadUrl="https://oc.know-center.tugraz.at/owncloud/index.php/s/Q3W8FqxsCFGRAMG?path=$uploadDir"
+
+echo "Creating backup directory $backupdir"
+mkdir $backupDir
+
+echo "Acquiring all room ids"
+mapfile result < <(docker exec -i nodechat mysql -u root --password=smoot -se "use nodechat;select id from room;")
+
+echo "Starting backup of messages for acquired room ids"
+for row in "${result[@]}";do
+	docker exec -i nodechat mysql -u root --password=smoot -se "use nodechat;select timestamp, content from message where roomid = ${row%$'\t'*};" > "$backupDir/conversation-${row}.csv"
+done
+
+echo "Backup step completed"
+
+if [ "$(ls -A $backupDir)" ]; then
+	echo "Creating directory on Nextcloud"
+	curl -u "Q3W8FqxsCFGRAMG:)5aYpg|dH8p7" -X MKCOL "https://oc.know-center.tugraz.at/owncloud/public.php/webdav/$uploadDir" -k
+	echo "Finished directory $uploadDir creation"
+	echo "Starting upload to Nextcloud $uploadDir"
+	export CLOUDSEND_PASSWORD=')5aYpg|dH8p7'
+        find $backupDir -iname '*.csv' -exec ./cloudsend.sh -p {} $uploadUrl \;
+	echo "Upload finished"
+else
+	echo "$backupDir is Empty, directory will be removed and not uploaded"
+	rm -r $backupDir
+fi
