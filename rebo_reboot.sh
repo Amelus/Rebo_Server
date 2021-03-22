8e0fcfe8334e321e6970f1f193323969b6bb1d78
Amel Hamidovic <hamidovic@student.tugraz.at>
2021-03-22 22:41:12 +0100

    Made reboot use no-cache

--- rebo_reboot.sh
+++ rebo_reboot.sh
@@ -4,7 +4,7 @@ cd /home/ahamidovic/Rebo_Server/
 /usr/local/bin/docker-compose stop
 sleep 15
 echo "Rebuilding Docker"
-/usr/local/bin/docker-compose build
+/usr/local/bin/docker-compose build --no-cache
 sleep 15
 echo "Starting container again"
 /usr/local/bin/docker-compose up -d
